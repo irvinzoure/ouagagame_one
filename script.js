58ee21384862604c9496549c978002229260bf60
irvin <irvin.zoure@gmail.com>
2018-04-27 14:48:40 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+var canvas = document.getElementById("ouagagame");
+var ctx = canvas.getContext("2d")
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
